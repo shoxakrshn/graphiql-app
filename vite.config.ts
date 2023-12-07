1800a719fd11ec71272e246cf5419061c2690ff8
shoxa <sllx.ok@gmail.com>
2023-12-07 15:23:21 +0500

    feat: install neccessary packages for the project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./vitest.setup.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      reportsDirectory: './src/tests/coverage',
+      all: true,
+      include: ['src/components/**/*.tsx'],
+      exclude: [],
+    },
+  },
+});
