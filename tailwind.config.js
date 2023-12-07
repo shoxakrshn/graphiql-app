bf760f5df1093fe72c9232346f2adf2e62a89e64
shoxa <sllx.ok@gmail.com>
2023-12-08 00:21:57 +0500

    feat: configure styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Mulish', 'Gilroy', 'system-ui', 'sans-serif'],
+        mono: ['FiraCode', 'Menlo', 'monospace'],
+      },
+    },
   },
   plugins: [],
 };
