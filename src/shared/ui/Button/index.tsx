1bfaa549bca953cfc7b953d8212361ec275f5ea6
Lizaveta <lizabeth.nik@gmail.com>
2023-12-08 17:40:14 +0200

    feat: add Button

--- src/shared/ui/Button/index.tsx
+++ src/shared/ui/Button/index.tsx
@@ -1,3 +1,24 @@
-export const Button = () => {
-  return <button>click</button>;
+import React from 'react';
+import { ButtonType } from '../../utils/data';
+import styles from './Button.module.scss';
+
+interface ButtonProps {
+  text: string;
+  onClick: () => void;
+  typeButton: ButtonType;
+}
+
+export const Button: React.FC<ButtonProps> = ({ 
+  text, 
+  onClick, 
+  typeButton = ButtonType.Filled
+}) => {
+  return (
+    <button 
+    className={typeButton === ButtonType.Filled ? styles.filled : styles.outlined} 
+    onClick={onClick}  
+    >
+      {text}
+    </button>
+  );
 };
---
+++ src/shared/utils/data.tsx
@@ -0,0 +1,4 @@
+export enum ButtonType {
+    Filled = ".filled",
+    Outlined = ".outlined",
+}
