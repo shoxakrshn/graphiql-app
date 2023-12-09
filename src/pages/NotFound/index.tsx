43260a7c4d363dcbe0276b4e4c15e3a21eaa1238
Lizaveta <lizabeth.nik@gmail.com>
2023-12-09 17:11:55 +0200

    feat: add 404-page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,5 +1,31 @@
+import { useNavigate } from 'react-router-dom';
+import { useLanguage } from '../../app/context/localizationContext/LocalizationContext';
+import { Button } from '../../shared/ui';
+import { eButtonType } from '../../shared/utils/data';
+import styles from './NotFound.module.scss';
+
 const NotFound = () => {
-  return <div>NotFound Page</div>;
+  const { t } = useLanguage();
+  const navigate = useNavigate();
+
+  const goHome = () => {
+    navigate('/');
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.content}>
+        <h1>404</h1>
+        <p>{t('error-404')}</p>
+        <Button
+          text={t('go-home')}
+          onClick={goHome}
+          typeButton={eButtonType.Filled}
+          type="button"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
