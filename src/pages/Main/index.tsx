bef6e5960496a1b6f3da88a81a8693560fd57e2e
Lizaveta <lizabeth.nik@gmail.com>
2023-12-09 12:02:58 +0200

    feat: add img for page welcome

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,5 +1,21 @@
+import loginImg from '../../app/assets/icons/welcome.svg';
+import styles from './Main.module.scss';
+
 const Main = () => {
-  return <div>Main Page</div>;
+  return (
+    <div className={styles.wrapper}>
+      <h1>Welcome</h1>
+      <div className={styles.content}>
+        <div className={styles.blockForm}>
+          <>TODO content</>
+        </div>
+        <div className={styles.loginIcon}>
+          <img src={loginImg} alt="Login" className={styles.icon} />
+        </div>
+      </div>
+    </div>  
+  )
+  
 };
 
 export default Main;
