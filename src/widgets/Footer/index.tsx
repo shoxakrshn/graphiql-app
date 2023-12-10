af747da28faa7dd9a26ee3a848b2aea3b523b0d5
shahzod222 <shahzodubaydullaev2206@gmail.com>
2023-12-10 12:13:25 +0500

    feat: add footer with links and make it adaptive

--- src/widgets/Footer/index.tsx
+++ src/widgets/Footer/index.tsx
@@ -1,3 +1,35 @@
+import gitIcon from '../../app/assets/icons/github.svg';
+import rslogo from '../../app/assets/icons/rslogo.svg';
+import styles from './Footer.module.scss';
+
+enum eGits {
+  shahzod = 'shahzod222',
+  shoha = 'shoxakrshn',
+  liza = 'LizavetaNik',
+}
+
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.footer}>
+      <div className={styles.gitIcons}>
+        <GithubLink name={eGits.liza} />
+        <GithubLink name={eGits.shahzod} />
+        <GithubLink name={eGits.shoha} />
+      </div>
+
+      <a href="https://rs.school/react/" target="_blank" rel="noreferrer">
+        <img src={rslogo} alt="RSSCHOOL" />
+      </a>
+
+      <p>© 2023 QraphiQL All Rights Reserved.</p>
+    </div>
+  );
+};
+
+const GithubLink = (props: { name: string }) => {
+  return (
+    <a href={`https://github.com/${props.name}`} target="_blank" rel="noreferrer">
+      <img src={gitIcon} />
+    </a>
+  );
 };
