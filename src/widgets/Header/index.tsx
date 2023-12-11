4de9e27a59d52e9cac1901346094f99859112c7e
shahzod222 <shahzodubaydullaev2206@gmail.com>
2023-12-11 18:24:49 +0500

    refactor: refactor header widget

--- src/widgets/Header/index.tsx
+++ src/widgets/Header/index.tsx
@@ -1,19 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import logo from '../../app/assets/icons/logo.svg';
-import burger from '../../app/assets/icons/burger.svg';
-import close from '../../app/assets/icons/close.svg';
 import { Link } from 'react-router-dom';
-import { Registration } from '../../shared/ui/Header Elements/Registration';
-import { SignedUserHeader } from '../../shared/ui/Header Elements/SignedUserHeader';
-import { LanguageToggler } from '../../shared/ui/Header Elements/LanguageTogler';
 import styles from './Header.module.scss';
+import { Burger, NavMenu } from '../../shared/ui';
 
 export const Header = () => {
   //temporary indicator of signed user
-  const isUserSigned = true;
+  const isUserSigned = false;
   //temporary indicator of signed user
-
-  const [menuOpen, setMenuOpen] = useState(false);
   const [isSticky, setIsSticky] = useState(false);
 
   const handleScroll = () => {
@@ -35,26 +29,11 @@ export const Header = () => {
           <img src={logo} alt="Logo" />
         </Link>
 
-        <div className={styles.desktopMenu}>
-          <LanguageToggler />
-          {isUserSigned ? <SignedUserHeader /> : <Registration />}
-        </div>
+        <NavMenu userStatus={isUserSigned} layout={'desktop'} />
 
-        {menuOpen ? (
-          <div className={styles.mobileMenuContent}>
-            <button className={styles.closeButton} onClick={() => setMenuOpen(false)}>
-              <img src={close} alt="Close Menu" />
-            </button>
-            <LanguageToggler />
-            {isUserSigned ? <SignedUserHeader /> : <Registration />}
-          </div>
-        ) : (
-          <div className={styles.mobileMenu}>
-            <button className={styles.menuButton} onClick={() => setMenuOpen(true)}>
-              <img src={burger} alt="Burger Menu" />
-            </button>
-          </div>
-        )}
+        <Burger>
+          <NavMenu userStatus={isUserSigned} layout={'mobile'} />
+        </Burger>
       </div>
     </header>
   );
