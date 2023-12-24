e33308763cb26b321380678f7deeb22a68b027dc
Lizaveta <lizabeth.nik@gmail.com>
2023-12-24 17:52:36 +0200

    fix: add graphgl-tag for format

--- src/shared/utils/functions.tsx
+++ src/shared/utils/functions.tsx
@@ -1,3 +1,5 @@
+import { parse, DocumentNode, print } from 'graphql';
+
 export function convertToPrettier(file: string): string {
   try {
     const parsedResponse = JSON.parse(file);
@@ -5,39 +7,17 @@ export function convertToPrettier(file: string): string {
       return JSON.stringify(parsedResponse, null, 2);
     }
   } catch (jsonError) {
-    const stringWithSpaces = file.replace(/{/g, ' { ').replace(/}/g, ' } ');
-    const cleanedString = stringWithSpaces.replace(/\s+/g, ' ').trim();
-    const countOpeningBraces = (cleanedString.match(/{/g) || []).length + 2;
-    let level = 0;
-    let formattedString = cleanedString
-      .replace(/(\{|\[|\() /g, (match, p1) => {
-        level += 2;
-        return `${p1}\n${' '.repeat(level)}`;
-      })
-      .replace(/ (\}|\]|\))/g, (match, p1) => {
-        level -= 2;
-        return `\n${' '.repeat(level)}${p1}`;
-      });
-
-    const matches = formattedString.match(/\{([^{}]+)\}/g);
-
-    if (matches) {
-      for (const match of matches) {
-        const wordsOnly = match.match(/\b\w+\b/g);
-        if (wordsOnly && wordsOnly.length > 1) {
-          const indentedWords = wordsOnly
-            .map((word) => ' '.repeat(countOpeningBraces) + word)
-            .join('\n');
-          formattedString = formattedString.replace(
-            match,
-            `{ \n${indentedWords}\n${' '.repeat(countOpeningBraces - 2)}}`,
-          );
-        }
-      }
-    }
-
-    return formattedString;
+    return formatGraphQL(file);
   }
 
   return file;
+}
+function formatGraphQL(query: string): string {
+  try {
+    const parsedQuery: DocumentNode = parse(query, { noLocation: true });
+    const formattedQuery = print(parsedQuery);
+    return `query ${formattedQuery}`;
+  } catch (error) {
+    return query;
+  }
 }
