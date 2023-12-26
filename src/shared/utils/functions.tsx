5c237a37363ec0e93518cd5c7cce11fb5eb2b3a1
Lizaveta <lizabeth.nik@gmail.com>
2023-12-26 16:18:23 +0200

    fix: fix prettier

--- src/shared/utils/functions.tsx
+++ src/shared/utils/functions.tsx
@@ -1,5 +1,3 @@
-import { parse, DocumentNode, print } from 'graphql';
-
 export function convertToPrettier(file: string): string {
   try {
     const parsedResponse = JSON.parse(file);
@@ -7,18 +5,74 @@ export function convertToPrettier(file: string): string {
       return JSON.stringify(parsedResponse, null, 2);
     }
   } catch (jsonError) {
-    return formatGraphQL(file);
+    return processGraphQLQuery(file);
   }
 
   return file;
 }
 
-function formatGraphQL(query: string): string {
-  try {
-    const parsedQuery: DocumentNode = parse(query, { noLocation: true });
-    const formattedQuery = print(parsedQuery);
-    return `query ${formattedQuery}`;
-  } catch (error) {
-    return query;
+const normalize = (str: string) => {
+  const special = [',', '.', ':'];
+  const removedSpace = str.split(' ').join('');
+
+  let result = '';
+
+  for (let i = 0; i <= removedSpace.length - 1; i += 1) {
+    const current = removedSpace[i];
+    const next = removedSpace[i + 1];
+
+    result += current;
+
+    if (special.includes(current)) {
+      result += ' ';
+    }
+
+    if (next === '{') {
+      result += ' ';
+    }
   }
-}
+
+  return result;
+};
+
+const processGraphQLQuery = (str: string) => {
+  const cleanedSpaces = normalize(str);
+  let result = '';
+  const indentSize = 2;
+  let indentLevel = 0;
+
+  for (let i = 0; i <= cleanedSpaces.length - 1; i += 1) {
+    const current = cleanedSpaces[i];
+    const next = cleanedSpaces[i + 1];
+    const prev = cleanedSpaces[i - 1];
+
+    result += current;
+
+    if (current === '{' && next !== '\n') {
+      indentLevel += 1;
+      result += '\n';
+      result += ' '.repeat(indentLevel * indentSize);
+    }
+
+    if (current === '\n' && prev === '{') {
+      indentLevel += 1;
+      result += ' '.repeat(indentLevel * indentSize);
+    }
+
+    if (current === '\n' && prev !== '{' && next !== '}') {
+      result += ' '.repeat(indentLevel * indentSize);
+    }
+
+    if (next === '}') {
+      indentLevel -= 1;
+
+      if (current !== '\n') {
+        result += '\n';
+      }
+
+      result += ' '.repeat(indentLevel * indentSize);
+    }
+  }
+
+  return result.replace(/query(?!\s)/, 'query ');
+};
