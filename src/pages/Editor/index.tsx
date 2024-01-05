5fcb6cde54235859bef33a86b38f76f3d03e46fe
Lizaveta <lizabeth.nik@gmail.com>
2024-01-05 10:51:18 +0200

    fix: refactoring modal document

--- src/pages/Editor/index.tsx
+++ src/pages/Editor/index.tsx
@@ -18,7 +18,7 @@ import { convertToPrettier } from '../../app/prettier/prettierApp';
 import { AppPrettier } from '../../app/assets/icons/AppPrettier';
 import styles from './Editor.module.scss';
 import { useAppSelector } from '../../app/store/hooks/hooks';
-import Modal from '../../shared/ui/modal/modal';
+import Modal from '../../shared/ui/Modal';
 
 type TabType = 'variables' | 'headers';
 
